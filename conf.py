be0c894d203d90e818af25298cce1d979e8bb14c
Rik Goldman <rikgoldman@gmail.com>
2013-01-23 08:04:15 -0500

    mod conf.py for rst2pdf

--- conf.py
+++ conf.py
@@ -287,7 +287,7 @@ epub_copyright = u'2013, Rik Goldman'
 # -- Options for PDF output --------------------------------------------------
 pdf_use_toc = True
 pdf_documents = [
-('index', u'heading', u'preface', u'prep_sem', u'year_1', u'year_2', u'year_3', u'year_4', u'anchor_texts', u'authentic', u'inventory', u'references'),
+('index', u'heading', u'LICENCE', u'preface', u'prep_sem', u'year_1', u'year_2', u'year_3', u'year_4', u'anchor_texts', u'authentic', u'inventory', u'references'),
 ]
 pdf_language = "en_US"
 pdf_use_index = True
